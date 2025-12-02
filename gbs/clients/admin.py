04e8c56bdc68b8ecc1f2efb7134d4d9a2d598cc6
Marissa <marissalamrii@gmail.com>
2025-12-02 16:28:18 +0100

    Change of the admin files for the superuser (don't forget to make one )

--- gbs/clients/admin.py
+++ gbs/clients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client, Dossiers, Documents, Prediagnostique
+
+admin.site.register(Client)
+admin.site.register(Dossiers)
+admin.site.register(Documents)
+admin.site.register(Prediagnostique)
 
 # Register your models here.
