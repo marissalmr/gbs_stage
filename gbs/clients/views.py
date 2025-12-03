baee6c1b5ca07c795ded5b7fd4d3c7f2b08ee9c9
Marissa <marissalamrii@gmail.com>
2025-12-03 14:46:05 +0100

    add view that clean siret and verify the length of it and raise error

--- gbs/clients/views.py
+++ gbs/clients/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
+from django import forms
 
+def clean_siret(self):
+    siret = self.cleaned_data['siret']
+    if len(siret)!=14 or siret.isdigit() :
+        raise forms.ValidationError("Le SIRET doit contenir 14 chiffres.")
