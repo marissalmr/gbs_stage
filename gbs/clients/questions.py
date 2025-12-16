c2a76081632d8dc7ff758fbd8dfe2012dcf23858
Marissa <marissalamrii@gmail.com>
2025-12-16 11:34:54 +0100

    Finish the dictionaries question

--- gbs/clients/questions.py
+++ gbs/clients/questions.py
@@ -56,18 +56,65 @@ QUESTIONS_DIAG = [
             ("50-75", "Entre 50 % et 75 %"),
             (">75", "Plus de 75 %"),
             ("100", "100 % sous-traité"),
-            ],        
+                    ],        
             },
     {
-    "id": 4,
-     "question" :  "Quel est votre chiffre d’affaires annuel approximatif ?", 
-     "choices": CA_CHOICES,
-     "type": "unique",
-     "show_if" : {
-         "question_id" : 2,
-         "value" : "non",
-         }
+        "id": 4,
+        "question" :  "Quel est votre chiffre d’affaires annuel approximatif ?", 
+        "choices": CA_CHOICES,
+        "type": "unique",
+        "show_if" : {
+                    "question_id" : 2,
+                    "value" : "non",
+                    }
+
      },
+
+     {
+        "id": 5,
+     "question" :  "Avez-vous déjà réglé des frais de dossier ou un bon de commande auprès de QUALIBAT en 2025 ?", 
+     "choices": {
+        [
+         ("oui", "Oui"),
+         ("non", "Non")
+        ]
+                }, 
+     },
+
+     {
+        "id": 6,
+     "question" :  "Avez-vous déjà suivi une formation RENOV’PERF (transverse et/ou modules métiers) ?", 
+     "choices": {
+        [
+         ("oui", "Oui"),
+         ("non", "Non")
+        ]
+                }, 
+     },
+     
+     {
+        "id": 7,
+     "question" :  "Combien d’ouvriers interviennent habituellement sur vos chantiers ?", 
+     "choices": {
+        [
+            ("moins_10", "Moins de 10 ouvriers"),
+            ("plus_10", "10 ouvriers ou plus"),
+        ]
+                },  
+     },
+
+     {
+        "id": 8,
+     "question" :  "Combien de chantiers de fourniture et pose d’isolants avez-vous déjà réalisés pour ces activités ?", 
+     "choices": {
+         [
+            ("0_2", "Moins de 3 chantiers"),
+            ("3_plus", "3 chantiers ou plus"),
+            ],
+                }, 
+     },
+
+
   
     ],
     
